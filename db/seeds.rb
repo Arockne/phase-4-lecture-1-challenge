4700bb8eccca7448665f3b355332dca7473ba94a
Daniel Calvin <arockne2003@yahoo.com>
2021-12-21 19:02:58 -0700

    Implement user and book seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+sam = User.create(username: 'sam', email: 'hello@yahoo.com', bio: 'Here I am')
+bob = User.create(username: 'bob', email: 'iambob@gmail.com', bio: 'I like turtles')
+
+codeschool = Book.create(title: 'codeschool', author: 'Morgan Lopes', description: 'Overcome Imposter Syndrome')
+bluesky = Book.create(title: 'bluesky', author: 'bluesky', description: 'to jot down some notes')
+hello = Book.create(title: 'itsme', author: 'I can see it in your eyes', description: 'Overcome Imposter Syndrome')
+
+
+sam.books = [codeschool, bluesky, hello]
+bob.books = [bluesky, codeschool]
