e02a5648fb1e7cd9496d8554d85bc488c067c670
Daniel Calvin <arockne2003@yahoo.com>
2021-12-21 18:47:37 -0700

    Implement index and show actions in books-controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,11 @@
 class BooksController < ApplicationController
+  def index
+    books = Book.all
+    render json: books, status: :ok
+  end  
+
+  def show
+    book = Book.find(params[:id])
+    render json: book, include: :users, status: :ok
+  end
 end
