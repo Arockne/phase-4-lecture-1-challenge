78a2ff371f7f5111b3202f152e59b4c66a8e1083
Daniel Calvin <arockne2003@yahoo.com>
2021-12-23 09:21:00 -0700

    validate attributes to be present and validate titles uniqueness with an author

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
   has_many :user_books
   has_many :users, through: :user_books
+
+  validates :title, :author, :description, presence: true
+  validates :title, uniqueness: { 
+    scope: :author,
+    message: 'can\'t have the same title twic with the same author'
+  }
 end
