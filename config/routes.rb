e22d10d6862afdd90b40ddfc17902c3eda7a284f
Daniel Calvin <arockne2003@yahoo.com>
2021-12-23 09:58:57 -0700

    Add routes four user_books

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :books, only: [:index, :show, :create]
+  resources :user_books, only: [:create, :index]
   resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
