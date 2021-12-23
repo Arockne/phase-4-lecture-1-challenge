f09bf2228004ddbe51524a60ea3d6057f4d10126
Daniel Calvin <arockne2003@yahoo.com>
2021-12-23 10:04:14 -0700

    Implement user_books_params and create action

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -4,4 +4,15 @@ class UserBooksController < ApplicationController
     user_books = current_user.books
     render json: user_books, status: :ok
   end
+
+  def create
+    user_book = current_user.books.create!(user_book_params)
+    render json: user_book, status: :created
+  end
+
+  private
+
+  def user_book_params
+    params.permit(:user_id, :book_id, :read)
+  end
 end
