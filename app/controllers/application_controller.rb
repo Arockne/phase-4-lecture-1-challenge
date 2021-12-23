786f70308fd92738e3e16d137313eb7660a16413
Daniel Calvin <arockne2003@yahoo.com>
2021-12-23 09:32:55 -0700

    Implement error handling for invalid records

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord:;InvalidRecord, with: :render_unprocessable_entity
+
+  private
+
+  def render_unprocessable_entity(invalid)
+    render json: invalid.record.full_messages, status: :unprocessable_entity
+  end
 end
