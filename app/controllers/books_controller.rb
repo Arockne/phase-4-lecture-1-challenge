73ccf200728ab2b1689f90f57d0b44bd79ee8286
Daniel Calvin <arockne2003@yahoo.com>
2021-12-23 09:22:27 -0700

    Implement strong book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,12 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     render json: book, include: :users, status: :ok
   end
+
+
+
+  private
+
+  def book_params
+    params.permit(:title, :author, :description, :cover_image_url)
+  end
 end
