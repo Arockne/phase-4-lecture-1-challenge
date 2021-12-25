b2cb20e214c02f6af18ff610aa37ea4d1c0d2bce
Daniel Calvin <arockne2003@yahoo.com>
2021-12-24 17:14:09 -0700

    Format book controller show action response

--- app/serializers/book_show_serializer.rb
+++ app/serializers/book_show_serializer.rb
@@ -1,3 +1,9 @@
 class BookShowSerializer < ActiveModel::Serializer
-  attributes :id
-end
+  attributes :id, :title, :author, :description, :cover_image_url, :finished_reading
+
+  has_many :readers
+  has_many :users_who_want_to_read
+
+
+# The BookShow will display the attributes and also the readers of the book
+# Advanced: try to read the ActiveModelSerializers guides on associations to figure out how you can also display the users who have actually read the book (not just added it to their reading list)
